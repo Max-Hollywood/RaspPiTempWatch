4a9190770e88829d90ea6a181318d20f2301d5c6
MaxHollywood <s3612990@student.rmit.edu.au>
2019-10-16 12:06:46 +1000

    Add initial script

---
+++ temperature.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# refs:
+# https://www.cyberciti.biz/faq/linux-find-out-raspberry-pi-gpu-and-arm-cpu-temperature-command/
+# https://linuxhint.com/raspberry_pi_temperature_monitor/
+# https://www.tutorialkart.com/bash-shell-scripting/bash-date-format-options-examples/
+
+printf "%-15s%13s\n" "TIMESTAMP" "TEMP(C)"
+printf "%20s\n" "----------------------------"
+
+while true
+do
+	temp=$(</sys/class/thermal/thermal_zone0/temp)
+	timestamp=$(date +'%a %F %H:%M')
+	colour="\e[1;33m%8s\e[m"
+	printf "%-15s" "$timestamp" 
+	printf "$colour\n" "$((temp/1000))c"
+	echo "$(date)" "$((temp/1000))c" >> temps.txt
+	sleep 60
+done
+
