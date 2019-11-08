fa220d1a7bf61b1b276b19469c5670f6205c51a5
Max-Hollywood <s3612990@student.rmit.edu.au>
2019-11-08 14:55:28 +1000

    Add colours and thresholds to output + green for normal + yellow for warm + red for hot

--- temperature.sh
+++ temperature.sh
@@ -6,6 +6,7 @@
 #		1.1 - add todo, version info, variables
 #		1.2 - colours
 #		1.3 - arguments
+#		1.4 - add quotes, more colours
 #
 # Usage:
 #	./temperature.sh
@@ -34,30 +35,50 @@
 printf "%-15s%13s\n" "TIMESTAMP" "TEMP(C)"
 printf "%20s\n" "----------------------------"
 
+# Normal temperature range is 0 to WARM
+# < WARM		= COL_NORMAL
+# WARM - HOT	= COL_WARM
+# > HOT			= COL_HOT
+WARM="45"
+HOT="55"
+
 # TODO: add coloured output codes
+COL_NORMAL="\e[1;32m%8s\e[0m"
+COL_WARM="\e[1;33m%8s\e[0m"
+COL_HOT="\e[1;31m%8s\e[0m"
+colour="$COL_NORMAL"
+
 # TODO: add options
 
 # Sleep time in seconds (argument or default is 60)
-SLP=60
+SLP="60"
 
 # Output file (argument or default is ~/Documents/temps.txt)
+# No quotes?
 OUTFILE=~/Documents/temps.txt
 
 while true
 do
-	temp=$(</sys/class/thermal/thermal_zone0/temp)
-	timestamp=$(date +'%a %F %H:%M')
+	temp="$(( $(</sys/class/thermal/thermal_zone0/temp) / 1000))"
 
-	colour="\e[1;33m%8s\e[m"
+	timestamp="$(date +'%a %F %H:%M')"
+
+	if [[ "$temp" -ge "$HOT" ]] ; then
+		colour="$COL_HOT"
+	elif [[ "$temp" -ge "$WARM" ]] ; then
+		colour="$COL_WARM"
+	else
+		colour="$COL_NORMAL"
+	fi
 
 	# TODO: have coloured output
 	# TODO: have options
 	# TODO: get heavy processes
 
 	printf "%-15s" "$timestamp" 
-	printf "$colour\n" "$((temp/1000))c"
+	printf "$colour\n" "$((temp))c"
 
-	echo "$(date)" "$((temp/1000))c" >> "$OUTFILE"
+	echo "$(date) $((temp))c" >> "$OUTFILE"
 
 	sleep "$SLP"
 done
